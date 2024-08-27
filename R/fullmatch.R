f12a5b4ab3a9db9c70a5c2f1701142d187a12fcd
Garrett Shipley <garrett.shipley7+github@gmail.com>
2024-08-27 17:30:15 -0400

    Update fullmatch.R

--- R/fullmatch.R
+++ R/fullmatch.R
@@ -1,20 +1,15 @@
-#' Match a pattern against the entire string
+#' @title Match a pattern against the entire string
 #'
-#' `re_fullmatch` checks whether each element of a character vector fully
-#' matches a specified pattern (regular expression). If the provided pattern is
-#' not already a compiled pattern object, it compiles it using `re_compile`. The
-#' function ensures that the entire string matches the pattern from start to
-#' end.
+#' @description `re_fullmatch` checks whether each element of a character vector fully matches a specified pattern
+#' (regular expression). If the provided pattern is not already a compiled pattern object, it compiles it using
+#' `re_compile`. The function ensures that the entire string matches the pattern from start to end.
 #'
 #' @param pattern A regular expression pattern or a compiled pattern object.
-#' @param string A character vector where each element is a string to be matched
-#' against the pattern.
-#' @param ... Additional arguments to be passed to `re_compile` if the pattern
-#' needs to be compiled.
-#' @return A list where each element is a character vector containing the full
-#' match for the corresponding element of `string`, or `character(0)` if there
-#' is no match.
-#' #' @inheritDotParams re_compile
+#' @param string A character vector where each element is a string to be matched against the pattern.
+#' @param ... Additional arguments to be passed to `re_compile` if the pattern needs to be compiled.
+#' @return A list where each element is a character vector containing the full match for the corresponding element of
+#' `string`, or `character(0)` if there is no match.
+#' @inheritDotParams re_compile
 #' @examples
 #' pattern <- re_compile("\\d{3}-\\d{2}-\\d{4}")
 #' re_fullmatch(pattern, "123-45-6789") # Full match
