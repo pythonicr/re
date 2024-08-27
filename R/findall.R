0949738629280f2f64d6da9806fd89c421ae7dea
Garrett Shipley <garrett.shipley7+github@gmail.com>
2024-08-27 17:29:16 -0400

    Update findall.R

--- R/findall.R
+++ R/findall.R
@@ -1,16 +1,14 @@
-#' Extract all occurrences of a pattern in a string
+#' @title Extract all occurrences of a pattern in a string
 #'
-#' `re_findall` extracts all occurrences of a specified pattern (regular
-#' expression) from each element of a character vector. If the provided pattern
-#' is not already a compiled pattern object, it compiles it using `re_compile`.
+#' @description `re_findall` extracts all occurrences of a specified pattern (regular expression) from each element of a
+#' character vector. If the provided pattern is not already a compiled pattern object, it compiles it using
+#' `re_compile`.
 #'
 #' @param pattern A regular expression pattern or a compiled pattern object.
-#' @param string A character vector where each element is a string from which to
-#' extract matches of the pattern.
-#' @param ... Additional arguments to be passed to `re_compile` if the pattern
-#' needs to be compiled.
-#' @return A list of character vectors, where each vector contains all the
-#' matches found in the corresponding element of `string`.
+#' @param string A character vector where each element is a string from which to extract matches of the pattern.
+#' @param ... Additional arguments to be passed to `re_compile` if the pattern needs to be compiled.
+#' @return A list of character vectors, where each vector contains all the matches found in the corresponding element of
+#' `string`.
 #' @inheritDotParams re_compile
 #' @examples
 #' pattern <- re_compile("\\b\\w+\\b")
