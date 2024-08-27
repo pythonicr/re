798f564126fb21a488e5f157bb2635090201e152
Garrett Shipley <garrett.shipley7+github@gmail.com>
2024-08-27 17:31:29 -0400

    Update search.R

--- R/search.R
+++ R/search.R
@@ -1,18 +1,14 @@
-#' Search for a pattern in a string
+#' @title Search for a pattern in a string
 #'
-#' `re_search` searches for occurrences of a specified pattern (regular
-#' expression) within each element of a character vector. If the provided
-#' pattern is not already a compiled pattern object, it compiles it using
+#' @description `re_search` searches for occurrences of a specified pattern (regular expression) within each element of
+#' a character vector. If the provided pattern is not already a compiled pattern object, it compiles it using
 #' `re_compile`.
 #'
 #' @param pattern A regular expression pattern or a compiled pattern object.
-#' @param string A character vector where each element is a string in which to
-#' search for the pattern.
-#' @param ... Additional arguments to be passed to `re_compile` if the pattern
-#' needs to be compiled.
-#' @return A list where each element is a character vector containing all
-#' matches found in the corresponding element of `string`. If no matches are
-#' found, the element will be `character(0)`.
+#' @param string A character vector where each element is a string in which to search for the pattern.
+#' @param ... Additional arguments to be passed to `re_compile` if the pattern needs to be compiled.
+#' @return A list where each element is a character vector containing all matches found in the corresponding element of
+#' `string`. If no matches are found, the element will be `character(0)`.
 #' @inheritDotParams re_compile
 #' @examples
 #' pattern <- re_compile("\\d+")
