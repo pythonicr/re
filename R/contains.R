75f8ece2560fd09420ca690eee61615fd98a8a52
Garrett Shipley <garrett.shipley7+github@gmail.com>
2024-08-27 17:25:49 -0400

    Update contains.R

--- R/contains.R
+++ R/contains.R
@@ -1,16 +1,14 @@
 #' Check if string contains a regular expression
 #'
-#' `re_contains` checks whether a specified pattern (regular expression) is
-#' found within each element of a character vector. If the provided pattern is
-#' not already a compiled pattern object, it compiles it using `re_compile`.
+#' @description `re_contains` checks whether a specified pattern (regular expression) is found within each element of a
+#' character vector. If the provided pattern is not already a compiled pattern object, it compiles it using
+#' `re_compile`.
 #'
 #' @param pattern A regular expression pattern or a compiled pattern object.
-#' @param string A character vector where each element is a string to be checked
-#' against the pattern.
-#' @param ... Additional arguments to be passed to `re_compile` if the pattern
-#' needs to be compiled.
-#' @return A logical vector of the same length as `string`, indicating whether
-#' each element contains a match for the pattern.
+#' @param string A character vector where each element is a string to be checked against the pattern.
+#' @param ... Additional arguments to be passed to `re_compile`.
+#' @return A logical vector of the same length as `string`, indicating whether each element contains a match for the
+#' pattern.
 #' @inheritDotParams re_compile
 #' @examples
 #' pattern <- re_compile("^abc", IGNORECASE)
